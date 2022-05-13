057886a3a23599089ca5898b340edcb5683170b4
Võ Duy <voduy@MacBook-Pro-cua-Vo-2.local>
2022-05-13 13:22:54 +0700

    message

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const MESSAGE = require("../controllers/messageController");
 
 router.post("/send-message", MESSAGE.sendMessage);
-router.get("/get-message", MESSAGE.getMessage);
+router.get("/get-message/:conversationId", MESSAGE.getMessage);
 
 module.exports = router;
